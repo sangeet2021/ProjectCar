ac906fb7c6be3a86ca612d46f0d0d69d696fb588
Sangeet Paudel <stiletdurden252@gmail.com>
2024-11-14 23:47:05 +0545

    Initial Commit
    Just added navbar component. No designs have been applied.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Project</h1>
+        <ul>
+            <li>about</li>
+            <li>this</li>
+            <li>that</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar;
