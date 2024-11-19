8b278da7b5cfe5dd6ac7c9b40ce3ea5e74b4947e
Sangeet Paudel <stiletdurden252@gmail.com>
2024-11-19 23:44:28 +0545

    Glass
    Added a glass backgroind and removed carousel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import Carousel from "./components/Carousel/Carousel";
+import Glass from "./components/Glass/Glass";
+
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className="main">
         <main>
           <Navbar />
-          <Carousel />
+          <Glass/>
         </main>
       </div>
     </>
---
+++ src/components/Glass/Glass.jsx
@@ -0,0 +1,14 @@
+import "./Glass.css";
+
+const Glass = () => {
+  return (
+    <div className="glass">
+        <h1>Project<span>Cars</span></h1>
+      <div className="desc">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Glass;
